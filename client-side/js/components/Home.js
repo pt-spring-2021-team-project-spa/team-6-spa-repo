424523563917887e90e69c7ec41a048bda5e4b5b
Shellejd <rochelle.dishmon@gmail.com>
2021-07-19 15:02:10 -0400

    NewHomePage with updates

---
+++ client-side/js/components/Home.js
@@ -0,0 +1,7 @@
+// import handsImg from '../../assets/colorful kids painted hands.jpg';
+export default function Home() {
+    return `
+    <h1>Explore Your World and Beyond!</h1>
+    <img class="page-image" src="assets/colorful kids painted hands.jpg" alt="painted hands">
+   `;
+}
