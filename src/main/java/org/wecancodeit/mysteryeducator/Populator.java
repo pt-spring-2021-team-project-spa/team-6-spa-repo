1fcbfa6a28beca9a8676296ae8e64720e43b0fd2
Patel10 <parth1029@yahoo.com>
2021-07-22 08:57:50 -0400

    populator v2

--- src/main/java/org/wecancodeit/mysteryeducator/Populator.java
+++ src/main/java/org/wecancodeit/mysteryeducator/Populator.java
@@ -28,19 +28,19 @@ public class Populator implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Subject Science = new Subject(1,"testing","Science","testing","testing");
+        Subject Science = new Subject("testing","Science");
         subjectRepo.save(Science);
 
 
-        Planet Mercury = new Planet(1,"Mercury","first");
-        Planet Venus = new Planet (2,"Venus","second");
-        Planet Earth = new Planet(3,"Earth","third");
-        Planet Mars = new Planet(4,"Mars","fourth");
-        Planet Jupiter = new Planet(5,"Jupiter","fifth");
-        Planet Saturn = new Planet(6,"Saturn ","sixth");
-        Planet Uranus = new Planet(7,"Uranus","seventh");
-        Planet Neptune = new Planet( 8,"Neptune","eighth");
-        Planet Pluto = new Planet (9,"Pluto","ninth");
+        Planet Mercury = new Planet("Mercury","first");
+        Planet Venus = new Planet ("Venus","second");
+        Planet Earth = new Planet("Earth","third");
+        Planet Mars = new Planet("Mars","fourth");
+        Planet Jupiter = new Planet("Jupiter","fifth");
+        Planet Saturn = new Planet("Saturn ","sixth");
+        Planet Uranus = new Planet("Uranus","seventh");
+        Planet Neptune = new Planet( "Neptune","eighth");
+        Planet Pluto = new Planet ("Pluto","ninth");
         planetRepo.save(Mercury);
         planetRepo.save(Venus);
         planetRepo.save(Earth);
@@ -51,6 +51,27 @@ public class Populator implements CommandLineRunner {
         planetRepo.save(Neptune);
         planetRepo.save(Pluto);
 
-        InterestingInfo planet1 = new InterestingInfo( 1,"testing","testing","testing","testing");
+        InterestingInfo planet1 = new InterestingInfo( "4.5 billion years","35.98 million mi","3,032 mi","3.285 × 10^23 kg");
+        InterestingInfo planet2 = new InterestingInfo("4.53 billion years","67.24 million mi","7,520 mi","4.87 × 1024 kg");
+        InterestingInfo planet3 = new InterestingInfo("4.54 billion years","91 million mi","7,917 mi","5.972 × 10^24 kg");
+        InterestingInfo planet4 = new InterestingInfo("4.60 billion years","155.6 million mi","4,212 mi","6.39 × 10^23 kg");
+        InterestingInfo planet5 = new InterestingInfo("4.60 billion years","468.9 million mi","86,881 mi","1.898 × 10^27 kg");
+        InterestingInfo planet6 = new InterestingInfo("4.50 billion years","923.49 million mi","72,367 mi","5.683 × 10^26 kg");
+        InterestingInfo planet7 = new InterestingInfo("4.50 billion years","1.83 billion mi","31,518 mi","8.681 × 10^25 kg");
+        InterestingInfo planet8 = new InterestingInfo("4.50 billion years","2.78 billion mi","30,599 mi","1.024 × 10^26 kg");
+        InterestingInfo planet9 = new InterestingInfo("4.46 billion years","3.7 billion mi","1,476 mi","1.30 x 10^22 kg");
+        interestingRepo.save(planet1);
+        interestingRepo.save(planet2);
+        interestingRepo.save(planet3);
+        interestingRepo.save(planet4);
+        interestingRepo.save(planet5);
+        interestingRepo.save(planet6);
+        interestingRepo.save(planet7);
+        interestingRepo.save(planet8);
+        interestingRepo.save(planet9);
+
+
+
+
     }
 }
