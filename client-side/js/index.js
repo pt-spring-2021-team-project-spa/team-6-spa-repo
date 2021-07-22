aed20dce7fa6b0ec33d203c1e88f2f6f73cb9b07
Shellejd <rochelle.dishmon@gmail.com>
2021-07-22 16:32:01 -0400

    fixed issue with build

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-//import handsImg from '../images/colorful kids painted hands.jpg';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
@@ -11,21 +9,10 @@ import crud from './crud/crud';
 // import pages: history, science, art
 
 
-=======
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Tutor1 from "./components/Tutor1";
-import Tutor2 from "./components/Tutor2";
-import Contact from "./components/ContactUs";
-import Form from "./components/Form";
-import AboutUs from "./components/AboutUs";
->>>>>>> main
 
 buildPage();
 
 function buildPage() {
-<<<<<<< HEAD
     header();
     footer();
     navHome();
@@ -33,37 +20,26 @@ function buildPage() {
     navScience();
     navHistory();
     navArt();
-=======
-  header();
-  footer();
-  navHome();
-  navTutor1();
-  navTutor2();
-  navContactUs();
-  navForm();
-  navAboutUs();
->>>>>>> main
 }
 
 function header() {
-  const headerElem = document.querySelector(".header");
-  headerElem.innerHTML = Header();
+    const headerElem = document.querySelector(".header");
+    headerElem.innerHTML = Header();
 }
 
 function footer() {
-  const footerElem = document.querySelector(".footer");
-  footerElem.innerHTML = Footer();
+    const footerElem = document.querySelector(".footer");
+    footerElem.innerHTML = Footer();
 }
 
 function navHome() {
-  const homeElem = document.querySelector(".nav-list__home");
-  homeElem.addEventListener("click", () => {
-    const app = document.querySelector("#app");
-    app.innerHTML = Home();
-  });
+    const homeElem = document.querySelector(".nav-list__home");
+    homeElem.addEventListener("click", () => {
+        const app = document.querySelector("#app");
+        app.innerHTML = Home();
+    });
 }
 
-<<<<<<< HEAD
 function navTutors() {
     const tutorsElem = document.querySelector('.nav-list__tutors');
     tutorsElem.addEventListener('click', () => {
@@ -109,44 +85,3 @@ function navArt() {
         });
     });
 }
-=======
-function navTutor1() {
-  const tutor1Elem = document.querySelector(".nav-list__tutor1");
-  tutor1Elem.addEventListener("click", () => {
-    const app = document.querySelector("#app");
-    app.innerHTML = Tutor1();
-  });
-}
-
-function navTutor2() {
-  const tutor2Elem = document.querySelector(".nav-list__tutor2");
-  tutor2Elem.addEventListener("click", () => {
-    const app = document.querySelector("#app");
-    app.innerHTML = Tutor2();
-  });
-}
-
-function navContactUs() {
-  const contactElem = document.querySelector(".nav-list__contact");
-  contactElem.addEventListener("click", () => {
-    const app = document.querySelector("#app");
-    app.innerHTML = Contact();
-  });
-}
-
-function navForm() {
-  const formElem = document.querySelector(".nav-list__form");
-  formElem.addEventListener("click", () => {
-    const app = document.querySelector("#app");
-    app.innerHTML = Form();
-  });
-}
-
-function navAboutUs() {
-  const aboutElem = document.querySelector(".nav-list__about");
-  aboutElem.addEventListener("click", () => {
-    const app = document.querySelector("#app");
-    app.innerHTML = AboutUs();
-  });
-}
->>>>>>> main
