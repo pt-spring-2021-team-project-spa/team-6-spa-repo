b3a44fa482441e9e9be36cc8cf82280cdda5c06f
sfafiade <thefaf17@gmail.com>
2021-07-23 19:14:52 -0400

    updated

--- client-side/js/components/ContactUs.js
+++ client-side/js/components/ContactUs.js
@@ -1,5 +1,68 @@
 export default function Contact() {
-    return  `<h1> Talk To Us !! </h1> 
+    return `
+    <style>
+ input[type=text], select, textarea {
+      width: 100%; 
+      padding: 12px; 
+      border: 1px solid #ccc; 
+      border-radius: 4px; 
+      box-sizing: border-box; 
+      margin-top: 6px; 
+      margin-bottom: 16px; 
+      
+    }
+    
+
+    input[type=submit] {
+      background-color: blue;
+      color: white;
+      padding: 12px 20px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+    
+   
+    input[type=submit]:hover {
+      background-color: gray;
+    }
+    
+    
+    .container {
+      border-radius: 5px;
+      background-color: red;
+      padding: 20px;
+    }
+
+    </style> 
+   
+    
+    
+    <h1> Talk To Us !! </h1> 
+    <div class="container">
+  
+  <div class="row">
+    <div class="column">
+    </div>
+    <div class="column">
+      <form action="/action_page.php">
+      
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email placeholder="Your email..">
+
+        <label for="email"> Your Thoughts </label>
+        <textarea id="subject" name="subject" placeholder="Write something.." style="height:170px"></textarea>
+        <input type="submit" value="Submit">
+      </form>
+    </div>
+  </div>
+</div>
 
     `
 }
--- client-side/js/components/Form.js
+++ client-side/js/components/Form.js
@@ -1,5 +1,58 @@
 export default function Form() {
-    return ` <h1>Schedule A Visit !! </h1>
-    
+  return ` <h1>Schedule A Visit !! </h1>
+    <div class="container">
+    <div class="card">
+       
+        <div class="time__container">
+            <div class="section">
+                <div class="box">
+                    1
+                </div><span>Date &amp; Time</span>
+            </div>
+            <form action="" class="form__time">
+                <div class="date">
+                    <label for="date">Date</label> <input id="date" type="date">
+                </div>
+                <div class="time">
+                    <label for="time">Time</label> <input id="time" type="time">
+                </div>
+                <div class="">
+                   
+                            </option>
+                        </select>
+                </div>
+            </form>
+        </div>
+        <div class="message__container">
+            <div class="section">
+                <div class="box">
+                    2
+                </div><span>Your Message</span>
+            </div>
+            <textarea placeholder="e.g. Please do .. Also, .." cols="50" rows="5"></textarea>
+        </div>
+        <div class="contact__container">
+            <div class="section">
+                <div class="box">
+                    3
+                </div><span>Contact Information</span>
+            </div>
+            <form action="" class="form__contact">
+                <div class="cname">
+                    <label for="cname">Name</label> <input placeholder="e.g. Richard Bovell" id="cname" type="text">
+                </div>
+                <div class="cnum">
+                    <label for="cnum">Phone Number</label> <input id="cnum" type="text">
+                </div>
+                <div class="email">
+                    <label for="cemail">Email</label> <input placeholder="e.g. rb@email.com" id="cemail" type="email">
+                </div>
+            </form>
+        </div>
+        <div class="form__confirmation">
+            <button>Confirm Information</button>
+        </div>
+    </div>
+</div>
     `;
 }
